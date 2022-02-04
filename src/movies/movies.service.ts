78052013513182d635fbc70b8fbfda0286dec440
Bogdan Covrig <bogcov@gmail.com>
2022-02-04 20:22:08 +0100

    Add TMDB Movie interface and types

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,22 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { Movie } from './movie.interface';
 import { TmdbService } from './tmdb/tmdb.service';
+import { TmdbMovie } from './tmdb/tmdbMovie.interface';
 
 @Injectable()
 export class MoviesService {
   constructor(private tmdbService: TmdbService) {}
 
   async getMovieById(id: number): Promise<Movie> {
-    console.log(await lastValueFrom(this.tmdbService.findAll()));
+    const tmdbResponse: TmdbMovie = await lastValueFrom(
+      this.tmdbService.getMovie(id),
+    );
 
     return {
-      id: id,
-      title: 'string',
-      tagline: 'string',
-      overview: 'string',
-      poster_path: 'string',
-      release_date: 'string',
-      runtime: 100,
+      id: tmdbResponse.id,
+      title: tmdbResponse.title,
+      tagline: tmdbResponse.tagline,
+      overview: tmdbResponse.overview,
+      poster_path: tmdbResponse.poster_path,
+      release_date: tmdbResponse.release_date,
+      runtime: tmdbResponse.runtime,
     };
   }
 }
--- src/movies/tmdb/tmdb.service.ts
+++ src/movies/tmdb/tmdb.service.ts
@@ -1,15 +1,17 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
-import { Movie } from '../movie.interface';
+import { TmdbMovie } from './tmdbMovie.interface';
 
 @Injectable()
 export class TmdbService {
   constructor(private httpService: HttpService) {}
 
-  findAll(): any {
+  getMovie(id: number): Observable<TmdbMovie> {
     return this.httpService
-      .get(`${process.env.TMDB_URL}/movie/550?api_key=${process.env.TMDB_KEY}`)
+      .get(
+        `${process.env.TMDB_URL}/movie/${id}?api_key=${process.env.TMDB_KEY}`,
+      )
       .pipe(map((response) => response.data));
   }
 }
