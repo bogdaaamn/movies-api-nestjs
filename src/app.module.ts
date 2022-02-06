647a276bd0f24fc1dfa731602ec7694a27d82993
Bogdan Covrig <bogcov@gmail.com>
2022-02-06 15:28:21 +0100

    Add base logger middleware for HTTP requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
@@ -6,10 +6,15 @@ import { TmdbService } from './tmdb/tmdb.service';
 import { HttpModule } from '@nestjs/axios';
 import { SearchController } from './search/search.controller';
 import { SearchService } from './search/search.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [MoviesController, SearchController],
   providers: [MoviesService, TmdbService, SearchService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
