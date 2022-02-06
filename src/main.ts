fbc6a35378eedaba2d37d320ce9a5e4780b0600f
Bogdan Covrig <bogcov@gmail.com>
2022-02-07 00:14:07 +0100

    Update main.ts SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Create the Nest AppModule
   const app = await NestFactory.create(AppModule);
 
+  // Create and configure Swagger for documentation
   const swaggerConfig = new DocumentBuilder()
     .setTitle('DEPT Movies API Nestjs')
     .setDescription(
@@ -16,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
+  // Start the express app on port 3000
   await app.listen(process.env.PORT || 3000);
 }
 
