40eae3f4eb1444e79497778a7a14c48d7ca3328b
Bogdan Covrig <bogcov@gmail.com>
2022-02-07 13:16:14 +0100

    Update the docs of /search

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -6,16 +6,32 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SearchResultPage } from './search.dto';
 import { SearchService } from './search.service';
 
 @Controller('search')
+@ApiTags('search')
 @UseInterceptors(CacheInterceptor)
 export class SearchController {
   constructor(private searchService: SearchService) {}
 
   @Get()
   @CacheTTL(3600)
+  @ApiOperation({
+    description:
+      "Search for movies. Gathers data from TMDB's Search Movies public route.",
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The search results of the query requested',
+    type: SearchResultPage,
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'The query or page parameters are invalid',
+  })
+  @ApiQuery({ name: 'page', required: false })
   async getSearchResult(
     @Query('query') query: string,
     @Query('page') page?: number,
--- src/search/search.dto.ts
+++ src/search/search.dto.ts
@@ -1,14 +1,57 @@
 class SearchResult {
+  /**
+   * The ID of the movie
+   * @example 550
+   */
   id: number;
+
+  /**
+   * The TMDB rating of the movie
+   * @example 8.4
+   */
   vote_average: number;
+
+  /**
+   * The relative path to the poster of the movie
+   * @example "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+   */
   poster_path: string;
+
+  /**
+   * The release date of the movie as a string
+   * @example "1999-10-15"
+   */
   release_date: string;
+
+  /**
+   * The title of the movie
+   * @example "Fight Club"
+   */
   title: string;
 }
 
 export class SearchResultPage {
+  /**
+   * The current page of results
+   * @example 1
+   */
   page: number;
+
+  /**
+   * The list of movie metadata matching the query
+   *
+   */
   results: SearchResult[];
+
+  /**
+   * The number of pages
+   * @example 100
+   */
   total_pages: number;
+
+  /**
+   * The number of movies matching the query
+   * @example 1982
+   */
   total_results: number;
 }
