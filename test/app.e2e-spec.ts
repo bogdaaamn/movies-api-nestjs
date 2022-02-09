0d72b09c34ded51f5d439151f8e4c9c2aea01eac
Bogdan Covrig <bogcov@gmail.com>
2022-02-10 00:43:27 +0100

    Update e2e tests to match last poster

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -22,7 +22,7 @@ describe('AppController (e2e)', () => {
       tagline: 'Mischief. Mayhem. Soap.',
       overview:
         'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
-      poster_path: '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+      poster_path: '/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg',
       release_date: '1999-10-15',
       runtime: 139,
       trailer: 'https://www.youtube.com/watch?v=6JnN1DmbqoU',
